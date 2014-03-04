67c982378519277ec8b78577ae7b2957c062d449
Craig Nakamoto <craig.nakamoto@claero.com>
2014-03-04 10:10:07 -0500

    recreating cl4auth after catastrophic failure

---
+++ config/cl4admin.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	// list of models in this module for cl4admin
+	'model_list' => array(
+		'user_admin' => 'User',
+		'auth_log' => 'Auth Log',
+		'auth_type' => 'Auth Type',
+		'group' => 'Group',
+		'group_permission' => 'Group - Permission',
+		'permission' => 'Permission',
+		'user_group' => 'User - Group',
+	),
+);
